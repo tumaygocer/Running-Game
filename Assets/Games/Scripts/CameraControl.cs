c374145b936969c5cdac59742325bac65a702311
Gocer615 <gtumayy21@gmail.com>
2022-10-28 16:45:42 +0300

    Feat: Camera Control

---
+++ Assets/Games/Scripts/CameraControl.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField] Vector3 distance;
+
+    public Transform target;
+   
+    private void Start()
+    {
+        distance = transform.position - target.transform.position;
+    }
+    private void LateUpdate()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, target.transform.position + distance, Time.deltaTime);
+    }
+
+}
