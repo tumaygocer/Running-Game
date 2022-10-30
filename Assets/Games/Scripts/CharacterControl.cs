2fe36be4e99d6cac0f5cb49c352f6425fbaf8350
Gocer615 <gtumayy21@gmail.com>
2022-10-30 14:45:33 +0300

    chore: platform deleted

--- Assets/Games/Scripts/CharacterControl.cs
+++ Assets/Games/Scripts/CharacterControl.cs
@@ -1,32 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class CharacterControl : MonoBehaviour
 {
 
-    public SpawnManager spawnManager;
-
-    void Start()
-    {
-
-    }
-
    
-    void Update()
-    {
-       
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        StartCoroutine(WaitTime());
-    }
-
-    IEnumerator WaitTime()
-    {
-        yield return new WaitForSeconds(3f);
-        spawnManager.SpawnTriggerEntered();
-
-    }
 }
