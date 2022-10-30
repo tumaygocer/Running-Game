57e5659cf1b2c67f20c118a37c94c8054352b763
Gocer615 <gtumayy21@gmail.com>
2022-10-30 23:36:43 +0300

    Feat: stagger animation

--- Assets/Games/Scripts/CharacterControl.cs
+++ Assets/Games/Scripts/CharacterControl.cs
@@ -10,17 +10,28 @@ public class CharacterControl : MonoBehaviour
     public GameObject stack;
     public Transform location;
 
+    public Animator anim;
+    private void Start()
+    {
+        anim = GetComponentInParent<Animator>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Collection"))
         {
-            Debug.Log("kskks");
+           
             //Destroy(other.gameObject);
             other.transform.parent = location.parent;
             other.gameObject.transform.position = location.transform.position;
             collection.Add(other.gameObject);
             float newy = collection[0].transform.position.y + offSet;
             other.gameObject.transform.position = new Vector3(location.transform.position.x, newy, location.transform.position.z);
+        }
+
+        if (other.gameObject.CompareTag("Stone"))
+        {           
+            anim.SetTrigger("Takýlma");           
         }
     }
 
